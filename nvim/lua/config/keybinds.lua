403f9986949981497c4a2a2e72bfbbd1682e33c1
NathanDSanta <natan.d.santa@gmail.com>
2025-04-10 19:09:34 +0200

    added lazy.nvim and some simple settings and keybinds

--- nvim/lua/config/keybinds.lua
+++ nvim/lua/config/keybinds.lua
@@ -1,1 +1,48 @@
+vim.g.mapleader = " "
 
+-- File explorer
+vim.keymap.set("n", "<leader>e", ":Ex<CR>");
+
+-- Movement
+vim.keymap.set("v", "<M-Up>", ":m '<-2<CR>gv=gv", desc = "Move current line up")
+vim.keymap.set("v", "<M-Down>", ":m '>+1<CR>gv=gv", desc = "Move current line down")
+vim.keymap.set("v", "<M-K>", ":m '<-2<CR>gv=gv", desc = "Move current line up")
+vim.keymap.set("v", "<M-J>", ":m '>+1<CR>gv=gv", desc = "Move current line down")
+
+vim.keymap.set("n", "<C-u>", "<C-u>zz", desc = "Keep cursor centered when moving up")
+vim.keymap.set("n", "<C-d>", "<C-d>zz", desc = "Keep cursor centerd when moving down")
+
+vim.keymap.set("n", "n", "nzzzv", desc = "Keep cursor centered when going to the next searched value")
+vim.keymap.set("n", "N", "Nzzzv", desc = "Keep cursor centered when going to the prev searched value")
+
+vim.keymap.set("n", "J", "mzJ`z", desc = "Keep cursor in place when using J")
+
+-- Deleting, copying and Pasting
+vim.keymap.set("n", "x", "\"_x", desc = "Do not copy when deleting a single character with x")
+
+vim.keymap.set({"n", "v"}, "<leader>y", "\"+y", desc = "Copy to system clipboard")
+
+
+vim.keymap.set("x", "<leader>p", "\"_dP", desc = "Keep the copied value when pasting over another word")
+
+-- Quick Fix navigation
+vim.keymap.set("n", "<C-Down>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-Up>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader><Down>", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader><Up>", "<cmd>lprev<CR>zz")
+
+
+
+--         _                   _             _  __          _     _           _     
+--        | |    ___  __ _  __| | ___ _ __  | |/ /___ _   _| |__ (_)_ __   __| |___ 
+--        | |   / _ \/ _` |/ _` |/ _ \ '__| | ' // _ \ | | | '_ \| | '_ \ / _` / __|
+--        | |__|  __/ (_| | (_| |  __/ |    | . \  __/ |_| | |_) | | | | | (_| \__ \
+--        |_____\___|\__,_|\__,_|\___|_|    |_|\_\___|\__, |_.__/|_|_| |_|\__,_|___/
+--                                                    |___/                         
+
+
+--##########
+--# DIRECT #
+--##########
+
+vim.keymap.set("n", "<leader>Y", "\"+Y", "Copy to system clipboard")
--- nvim/lua/config/settings.lua
+++ nvim/lua/config/settings.lua
@@ -1,1 +1,40 @@
+local opt = vim.opt
 
+opt.guicursor = ""
+
+-- Line numbers
+opt.nu = true
+opt.relativenumber=true
+
+-- Indentation
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.expandtab = true
+
+-- Text wrap
+opt.wrap = false
+
+-- File
+opt.swapfile = false
+opt.backup = false
+opt.undodir = os.getenv("HOME") .. "/.nvim/undodir"
+opt.undofile = true
+
+-- Search
+opt.hlsearch = false
+opt.incsearch = true
+
+-- Colors
+opt.termguicolors = true
+
+-- Scroll
+opt.scrolloff = 9
+opt.signcolumn = "yes"
+
+-- To make the editor feel more responsive, supposedly
+opt.updatetime = 250
+
+-- If i want to have a border at 80 characters
+-- opt.colorcolumn = "80"
