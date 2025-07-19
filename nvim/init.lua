7785b660d2c3d09ec1a0b9adccc73c8886c73d47
NathanDSanta <natan.d.santa@gmail.com>
2025-07-19 16:13:50 +0200

    switched to caelestia desktop

--- nvim/init.lua
+++
@@ -1,5 +0,0 @@
-require("config.lazy")
-require("config.settings")
-require("config.keybinds")
-require("config.commands")
-require("config.autocmds")
--- nvim/lua/config/commands.lua
+++
@@ -1,3 +0,0 @@
-vim.api.nvim_create_user_command("BufferFormat", function()
-	require("conform").format({ async = true, lsp_format = "fallback" })
-end, { nargs = 0, desc = "Format buffer" })
